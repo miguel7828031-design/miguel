e21ab84893e70d19f1c5e1d9c55990201bc55e1b
miguel7828031-design <miguel.7828031@aluno.mg.gov.br>
2026-03-27 07:42:41 -0300

    Update and rename README.md to HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,6 @@
+public class HelloWorld {
+public static void main(String[] args) {
+System.out.println("Olá, Mundo! Meu nome é Miguel e este é meu primeiro programa no GitHub.");
+System.out.println("Turma: 2º1");
+}
+}
